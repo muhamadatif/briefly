344bad8741affedbe9e6e192072fb466ad181f0d
muhamadatif <mohamedabotreka40@gmail.com>
2025-05-20 00:46:19 +0300

    add the header section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="">
-      <Button variant="outline" size="lg">
+      <Button variant="outline" size="lg" className="">
         Shadcn Button
       </Button>
     </div>
